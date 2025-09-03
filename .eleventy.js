b4e9c6dff119ce71b5a00663a8cdd7484024b176
a.tabidze <register44@gmail.com>
2025-09-03 15:23:59 +0400

    aaa

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,5 @@
 module.exports = function(eleventyConfig) {
-  // ეს ხაზი აკოპირებს assets საქაღალდეს (CSS, სურათები და ა.შ.)
   eleventyConfig.addPassthroughCopy("src/assets");
-  
-  // ეს ხაზი აკოპირებს admin საქაღალდეს
   eleventyConfig.addPassthroughCopy("src/admin");
 
   return {
