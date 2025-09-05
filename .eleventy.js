d79491d16180b696de96d5156af0652a18995048
a.tabidze <register44@gmail.com>
2025-09-05 13:11:55 +0400

    adminpanelupdate

--- .eleventy.js
+++ .eleventy.js
@@ -2,18 +2,18 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets");
   eleventyConfig.addPassthroughCopy("src/admin");
 
-  eleventyConfig.addCollection("categories", function(collectionApi) {
-    return collectionApi.getFilteredByTag("categories").sort((a, b) => {
+  eleventyConfig.addCollection("catalogs", function(collectionApi) {
+    return collectionApi.getFilteredByGlob("src/catalogs/*.md").sort((a, b) => {
         return (a.data.sort_order || 99) - (b.data.sort_order || 99);
     });
   });
 
+  eleventyConfig.addCollection("details", function(collectionApi) {
+    return collectionApi.getFilteredByTag("details");
+  });
+
   return {
-    dir: {
-      input: "src",
-      includes: "_includes",
-      output: "_site"
-    },
+    dir: { input: "src", includes: "_includes", output: "_site" },
     templateFormats: ["md", "njk", "html"],
     markdownTemplateEngine: "njk",
   };
