52f1403046efd872a8d0ca5a73e71acdf4937379
a.tabidze <register44@gmail.com>
2025-09-05 12:55:01 +0400

    Final fix for CMS slug generation and styles

--- .eleventy.js
+++ .eleventy.js
@@ -3,9 +3,14 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/admin");
 
   eleventyConfig.addCollection("catalogs", function(collectionApi) {
-    return collectionApi.getFilteredByGlob("src/catalogs/*.md").sort((a, b) => {
+    return collectionApi.getFilteredByTag("catalogs").sort((a, b) => {
         return (a.data.sort_order || 99) - (b.data.sort_order || 99);
     });
+  });
+
+  // Eleventy-ს ვეუბნებით, რომ details კოლექციაც არსებობს
+  eleventyConfig.addCollection("details", function(collectionApi) {
+    return collectionApi.getFilteredByTag("details");
   });
 
   return {
