cfe839f1fb1205bdb97e65f65c28a1744bb5bf66
a.tabidze <register44@gmail.com>
2025-09-05 13:22:26 +0400

    adminpanel

--- .eleventy.js
+++ .eleventy.js
@@ -2,14 +2,10 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets");
   eleventyConfig.addPassthroughCopy("src/admin");
 
-  eleventyConfig.addCollection("catalogs", function(collectionApi) {
-    return collectionApi.getFilteredByGlob("src/catalogs/*.md").sort((a, b) => {
+  eleventyConfig.addCollection("categories", function(collectionApi) {
+    return collectionApi.getFilteredByTag("categories").sort((a, b) => {
         return (a.data.sort_order || 99) - (b.data.sort_order || 99);
     });
-  });
-
-  eleventyConfig.addCollection("details", function(collectionApi) {
-    return collectionApi.getFilteredByTag("details");
   });
 
   return {
--- src/catalogs/test.md
+++
@@ -1,9 +0,0 @@
----
-slug: test
-title: ჰელ დესკი
-sort_order: 0
-services:
-  - name: სერვის 1
-  - name: სერვის 2
-tags: catalogs
----
