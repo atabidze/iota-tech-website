2d8e0b376835b873a9138e4c1cd393c18d07094b
a.tabidze <register44@gmail.com>
2025-09-09 11:11:54 +0400

    update

--- .eleventy.js
+++ .eleventy.js
@@ -3,9 +3,11 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/admin");
 
   eleventyConfig.addCollection("services", function(collectionApi) {
-    return collectionApi.getFilteredByTag("services").sort((a, b) => {
-        return (a.data.sort_order || 99) - (b.data.sort_order || 99);
-    });
+    // ეს არის სწორი, ერთადერთი return, რომელიც ჯერ ფილტრავს და შემდეგ ალაგებს.
+    return collectionApi.getFilteredByTag("services")
+      // ვფილტრავთ კოლექციას, რათა წაიშალოს ყველა ჩანაწერი, რომელსაც არ აქვს სათაური.
+      .filter(item => item.data.title && item.data.title.trim() !== "")
+      .sort((a, b) => (a.data.sort_order || 99) - (b.data.sort_order || 99));
   });
 
   return {
