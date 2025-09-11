604dc6d159231d542c13f6deea03fa15ca15e03a
a.tabidze <register44@gmail.com>
2025-09-11 21:50:08 +0400

    updateKAEN1

---
+++ src/_data/en.js
@@ -0,0 +1,9 @@
+module.exports = {
+  nav: {
+    home: "Home",
+    services: "Services",
+    about: "About Us",
+    contact: "Contact"
+  },
+  lang_label: "Eng"
+};
---
+++ src/_data/ka.js
@@ -0,0 +1,9 @@
+module.exports = {
+  nav: {
+    home: "მთავარი",
+    services: "სერვისები",
+    about: "ჩვენ შესახებ",
+    contact: "კონტაქტი"
+  },
+  lang_label: "ქარ"
+};
