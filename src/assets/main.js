3b913238dd874a97dc2517fea3b154f9490a9683
a.tabidze <register44@gmail.com>
2025-09-19 16:55:29 +0400

    update lines36

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,18 +1,19 @@
-// ეს კოდი იმუშავებს მხოლოდ იმ გვერდზე, სადაც #vanta-hero-bg ელემენტი არსებობს.
-if (document.getElementById('vanta-hero-bg')) {
-  VANTA.FOG({
-    el: "#vanta-hero-bg",
-    mouseControls: true,
-    touchControls: true,
-    gyroControls: false,
-    minHeight: 200.00,
-    minWidth: 200.00,
-    highlightColor: 0x2300ff, // მთავარი ლურჯი ფერი
-    midtoneColor: 0x250072,   // შუალედური მუქი ლურჯი
-    lowlightColor: 0x200040,  // ყველაზე მუქი იისფერი-ლურჯი
-    baseColor: 0x121212,      // ფონის ფერი, ემთხვევა საიტის ფონს
-    blurFactor: 0.60,
-    speed: 1.20,
-    zoom: 0.80
-  });
-}
+document.addEventListener('DOMContentLoaded', () => {
+  if (document.getElementById('vanta-hero-bg')) {
+    VANTA.FOG({
+      el: "#vanta-hero-bg",
+      mouseControls: true,
+      touchControls: true,
+      gyroControls: false,
+      minHeight: 200.00,
+      minWidth: 200.00,
+      highlightColor: 0x2300ff,
+      midtoneColor: 0x250072,
+      lowlightColor: 0x200040,
+      baseColor: 0x121212,
+      blurFactor: 0.60,
+      speed: 1.20,
+      zoom: 0.80
+    });
+  }
+});
